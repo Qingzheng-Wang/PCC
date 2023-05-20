07f2a4d72ffa99bca204c018ffeb9bb3793fdb08
Qingzheng Wang <QingzhengWang@whu.edu.cn>
2023-05-20 17:42:49 +0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 # from parser import Node,build_ast
 from other.function import if_num 
 from LL import analysis
-import sys, os, re
+import sys, os
 sys.path.append(os.pardir)
 from lexer import word_list
 
@@ -18,10 +18,10 @@ operator = {
 特殊的自定义四元式语法：
     1.  (code_block, 0, 0, block1)   代码块开始标记
     2.  (j, 0, 0, , +2)              跳转语句，往后跳两行
-    3.  (j<, ａ, b, block1)          条件跳转 if(a<b) then　jmp block1
+    3.  (j<, a, b, block1)          条件跳转 if(a<b) then　jmp block1
     4.  (print, 0, 0, a)             打印变量ａ
 """
-class Mnode:
+class MNode:
     def __init__(self, op="undefined", a1=None, a2=None, re=None):
         self.op = op
         self.arg1 = a1
@@ -36,7 +36,7 @@ class Mnode:
 
 """
 两个全局 mid_result 存放四元式对象
-tmp记录零时变量id
+tmp记录临时变量id
 """
 mid_result = []
 while_flag = []
@@ -47,7 +47,6 @@ type_flag = ""
 """
 递归遍历语法树
 遇到相应非终结符做相应处理，遇到终结符返回终结符，其他字符递归处理其子节点
-
 """
 def view_astree(root, ft=None):
     global type_flag
@@ -57,13 +56,13 @@ def view_astree(root, ft=None):
         return
     elif len(root.child) == 0 and root.text != None:
         return root.text
-    if root.type == "L":
+    if root.type == "L": # 这里的type和LL里语法树节点的type一致，如果是非终结符，则type是其本身
         math_op(root)
-    elif root.type == "Pan":
+    elif root.type == "Pan": # "Pan"表示条件判断语句
         judge(root)
     elif root.type == "OUT":
         out(root)
-    else:
+    else: # 遇到其他非终结符则递归调用其子节点
         re = ""
         for c in root.child:
             cre = view_astree(c)
@@ -84,24 +83,24 @@ def math_op(root, ft=None):
     变量声明语句，两种情况
     1. 直接赋值
     2. 不赋值
+    (=, a, 0, c)的含义是：c ::= a
     """
-    if root.type == "L":
-        
-        c1 = root.child[1]
-        if len(c1.child) == 1:
-            mid_result.append(Mnode("=",0,0,math_op(root.child[0].child[0])))
-        elif c1.child[0].type == "=":
-            mid_result.append(Mnode("=",math_op(c1),0,math_op(root.child[0].child[0])))
-        else:
-            if len(c1.child[1].child) >1:
-                cc1 = c1.child[1]
-                mid_result.append(Mnode("=",math_op(cc1),0,math_op(root.child[0].child[0]) +"[]" + math_op(c1.child[0])))
-            if math_op(root.child[0].child[0]) not in arr:
+    if root.type == "L": # L -> M LM
+        c1 = root.child[1] # c1是LM
+        if len(c1.child) == 1: # 此时c1.child是null LM -> null
+            mid_result.append(MNode("=", 0, 0, math_op(root.child[0].child[0]))) # root.child[0].child[0]是parameter，此时是声明语句，不赋值
+        elif c1.child[0].type == "=": # LM -> = FE
+            mid_result.append(MNode("=", math_op(c1), 0, math_op(root.child[0].child[0])))
+        else: # LM -> Size AM
+            if len(c1.child[1].child) >1: # AM -> = E，该情况是数组赋值，类似s[1] = 1 + 2, 2
+                cc1 = c1.child[1] # E
+                mid_result.append(MNode("=", math_op(cc1), 0, math_op(root.child[0].child[0]) + "[]" + math_op(c1.child[0])))
+            if math_op(root.child[0].child[0]) not in arr: # 将math_op(root.child[0].child[0])添加到数组表中
                 arr[math_op(root.child[0].child[0])] = [math_op(c1.child[0]), type_flag]
                 type_flag = ""
     elif root.type == "ET" or root.type == "TT":
-        if len(root.child) > 1:
-            op = Mnode(math_op(root.child[0]))
+        if len(root.child) > 1: # ET -> + T ET / ET -> - T ET
+            op = MNode(math_op(root.child[0]))
             arg1 = math_op(root.child[1])
             if if_num(arg1) and if_num(ft):
                 return str(operator[op](int(arg1), int(ft)))
@@ -112,7 +111,7 @@ def math_op(root, ft=None):
             """
             t = "T" + str(tmp)
             tmp += 1
-            mid_result.append(Mnode(op, arg1, ft,t))
+            mid_result.append(MNode(op, arg1, ft, t))
             ct = math_op(root.child[2], t)
             if ct != None:
                 return ct
@@ -134,7 +133,7 @@ def math_op(root, ft=None):
 
             t = "T" + str(tmp)
             tmp += 1
-            mid_result.append(Mnode(op, arg1, arg2,t))
+            mid_result.append(MNode(op, arg1, arg2, t))
             ct = math_op(root.child[1].child[2], t)
             if ct != None:
                 return ct
@@ -178,7 +177,7 @@ def judge(root):
             """
             cur = len(mid_result)
             while_flag.append([True,cur])
-            mid_result.append(Mnode("code_block", 0, 0, "W" + str(cur)))
+            mid_result.append(MNode("code_block", 0, 0, "W" + str(cur)))
     if root.type == "Pbc":
         """
         判断语句括号中的的两种情况
@@ -187,9 +186,9 @@ def judge(root):
         """
         Pm = root.child[1].child
         if len(Pm) == 1:
-            mid_result.append(Mnode("j=", 1, math_op(root.child[0]),"code"+str(len(mid_result)+1)))
+            mid_result.append(MNode("j=", 1, math_op(root.child[0]), "code" + str(len(mid_result) + 1)))
         else:
-            mid_result.append(Mnode("j"+judge(Pm[0]), math_op(root.child[0]), math_op(Pm[1]),"code"+str(len(mid_result)+1)))
+            mid_result.append(MNode("j" + judge(Pm[0]), math_op(root.child[0]), math_op(Pm[1]), "code" + str(len(mid_result) + 1)))
         return
     if root.type == "Pro":
         """
@@ -205,12 +204,12 @@ def judge(root):
         w = while_flag.pop()
         code_block = len(mid_result)
         code = "block" + str(code_block)
-        mid_result.append(Mnode("j",0, 0,code))
-        mid_result.append(Mnode("code_block",0,0,"code"+str(code_block)))
+        mid_result.append(MNode("j", 0, 0, code))
+        mid_result.append(MNode("code_block", 0, 0, "code" + str(code_block)))
         view_astree(root)
         if w[0] == True:
-            mid_result.append(Mnode("j",0,0,"W"+str(w[1])))    
-        mid_result.append(Mnode("code_block",0,0,code))
+            mid_result.append(MNode("j", 0, 0, "W" + str(w[1])))
+        mid_result.append(MNode("code_block", 0, 0, code))
         code_block += 1
         return
     else:
@@ -231,7 +230,7 @@ def out(root):
         return
     elif root.type == "V":
         if len(root.child) <= 1:
-            mid_result.append(Mnode("print", '-1', '-1', '-1'))
+            mid_result.append(MNode("print", '-1', '-1', '-1'))
             return
         else:
             name = [math_op(root.child[1])]
@@ -240,7 +239,7 @@ def out(root):
                 name.append(math_op(V.child[1]))
                 V = V.child[2]
             name.extend(['-1','-1','-1'])
-            mid_result.append(Mnode("print", name[0], name[1], name[2]))
+            mid_result.append(MNode("print", name[0], name[1], name[2]))
     else:
         for c in root.child:
             out(c)
